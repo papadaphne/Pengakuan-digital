4fb857fe9f4e21a200ddec8df988dcdbbd3a2f53
papadaphne <arinkumanja76@gmail.com>
2025-07-30 16:27:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 document.getElementById("start").addEventListener("click", async () => {
-  const res = await fetch("https://your-backend-url/create-payment", { method: "POST" });
+  const res = await fetch("https:/https://pengakuan-digital-1.onrender.com-payment", { method: "POST" });
   const data = await res.json();
   if (data.redirect_url) {
     window.location.href = data.redirect_url;
